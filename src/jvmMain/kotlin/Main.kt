caf6df5b3660bc1060c2bf6760aa1941c2136651
Marçal <marcal.asbo@gmail.com>
2022-12-12 16:20:39 +0100

    2.13 Creación de la UI del listado de notas

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,19 +1,30 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import Note.Type
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 class AppState {
-    val text = mutableStateOf("")
-    val buttonEnabled: Boolean
-        get() = text.value.isNotEmpty()
+    val notes = mutableStateOf(getNotes())
 }
 
 @Composable
@@ -21,20 +32,40 @@ class AppState {
 fun App(appState: AppState) {
 
     MaterialTheme {
-        Column {
-            TextField(
-                value = appState.text.value,
-                onValueChange = { newText -> appState.text.value = newText }
-            )
-            Text(text = buildMessage(appState.text.value))
-            Button(onClick = { appState.text.value = "" }, enabled = appState.buttonEnabled) {
-                Text("Clean")
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(appState.notes.value) { note ->
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(0.8f)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Row {
+                            Text(
+                                text = note.title,
+                                style = MaterialTheme.typography.h6,
+                                modifier = Modifier.weight(1f)
+                            )
+                            if (note.type == Type.AUDIO) {
+                                Icon(
+                                    imageVector = Icons.Default.Mic,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(note.description)
+                    }
+                }
             }
         }
     }
 }
-
-fun buildMessage(message: String) = "Hello $message"
 
 fun main() = application {
     val appState = AppState()
