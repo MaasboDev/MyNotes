63e2646505d8a4fcbb60bc7f74b9649fd92a9e18
Marçal <marcal.asbo@gmail.com>
2022-12-12 17:45:20 +0100

    2.14 Lambdas

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import Note.Type
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,57 +11,86 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlin.concurrent.thread
 
 class AppState {
-    val notes = mutableStateOf(getNotes())
+    val state = mutableStateOf(UiState())
+
+    fun loadNotes() {
+        thread {
+            state.value = UiState(loading = true)
+            getNotes { state.value = UiState(notes = it, loading = false) }
+        }
+    }
+
+    data class UiState(
+        val notes: List<Note> = emptyList(),
+        val loading: Boolean = false
+    )
 }
 
 @Composable
 @Preview
 fun App(appState: AppState) {
 
+    LaunchedEffect(true) {
+        appState.loadNotes()
+    }
+
     MaterialTheme {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(appState.notes.value) { note ->
-                Card(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth(0.8f)
+        Box(contentAlignment = Alignment.Center) {
+            if (appState.state.value.loading) {
+                CircularProgressIndicator()
+            }
+            NotesList(appState.state.value.notes)
+        }
+    }
+}
+
+@Composable
+private fun NotesList(notes: List<Note>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(notes) { note ->
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(0.8f)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
                 ) {
-                    Column(
-                        modifier = Modifier.padding(16.dp)
-                    ) {
-                        Row {
-                            Text(
-                                text = note.title,
-                                style = MaterialTheme.typography.h6,
-                                modifier = Modifier.weight(1f)
+                    Row {
+                        Text(
+                            text = note.title,
+                            style = MaterialTheme.typography.h6,
+                            modifier = Modifier.weight(1f)
+                        )
+                        if (note.type == Type.AUDIO) {
+                            Icon(
+                                imageVector = Icons.Default.Mic,
+                                contentDescription = null
                             )
-                            if (note.type == Type.AUDIO) {
-                                Icon(
-                                    imageVector = Icons.Default.Mic,
-                                    contentDescription = null
-                                )
-                            }
                         }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(note.description)
                     }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(note.description)
                 }
             }
         }
--- src/jvmMain/kotlin/Note.kt
+++ src/jvmMain/kotlin/Note.kt
@@ -4,10 +4,14 @@ data class Note(val title: String, val description: String, val type: Type) {
     enum class Type { TEXT, AUDIO }
 }
 
-fun getNotes(): List<Note> = (1..10).map {
-    Note(
-        "Title $it",
-        "Description $it",
-        if (it % 3 == 0) Type.AUDIO else Type.TEXT
-    )
+fun getNotes(callback: (List<Note>) -> Unit) {
+    Thread.sleep(2000)
+    val notes = (1..10).map {
+        Note(
+            "Title $it",
+            "Description $it",
+            if (it % 3 == 0) Type.AUDIO else Type.TEXT
+        )
+    }
+    callback(notes)
 }
